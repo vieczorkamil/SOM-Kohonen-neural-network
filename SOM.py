38ba70b3e440d22dfda45c611adfc9a539b95912
vieczorkmail <vieczorkamil@gmail.com>
2023-03-23 14:44:11 +0100

    Finish train step of network. SOM works. Add data with sample to test

--- SOM.py
+++ SOM.py
@@ -1,39 +1,40 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import shutil 
+import glob
+import os
+import re
+
 
 class SOM:
     POINTS = None
     NN     = None
     def __init__(self, inputPoints: np.ndarray, nnSize: int, learningRate: float, epochs: int=10000) -> None:
-        self.POINTS = self.normalize(inputPoints)
+        self.POINTS = self._normalize(inputPoints)
         self.nnSize = nnSize
         self.n = nnSize
         self.learningRate = learningRate
         self.epochs = epochs
 
-    def normalize(self, array: np.ndarray) -> np.ndarray:
-        # return (array - np.min(array)) / (np.max(array) - np.min(array))
-        self.norms = np.linalg.norm(array, axis=0)
-        return array / self.norms
-        # return array / np.linalg.norm(array, axis=0)
-
-    def denormalize(self, array: np.ndarray) -> np.ndarray:
-        return array * self.norms
-
-    def _initNetwork(self) -> np.ndarray:
-        self.NN = np.random.rand(self.nnSize, 2)
-        return self.NN
     
-    def train(self):
+    def train(self, report: bool=False, reportPath: str="./report"):
+        if report:
+            if os.path.exists(reportPath):
+                shutil.rmtree(reportPath) 
+            os.makedirs(reportPath)
+            name = f"{reportPath}/epoch 0.png"
+            self._plotPoints(name)
+
         self._initNetwork()
-        for i in range(self.epochs):
-            print(f"\t Epoch {i}/{self.epochs}", end="\r")
-            point = self.randomSample()
-            winnerIndex = self.selectWinner(point)
-            gaussian = self.getNeighborhood(winnerIndex, self.n//8)
+        for i in range(1, self.epochs + 1):
+            print(f"\t Training in progress. Epoch {i}/{self.epochs}", end="\r")
+
+            point = self._randomSample()
+            winnerIndex = self._selectWinner(point)
+            gaussian = self._getNeighborhood(winnerIndex, self.n//8)
             self.NN += gaussian[:,np.newaxis] * self.learningRate * (point - self.NN)
 
-            # print(f"\t NN {self.NN[0][0]}/{self.NN[0][1]}", end="\r")
             # Decay the variables
             self.learningRate = self.learningRate * 0.99997
             self.n = self.n * 0.9997
@@ -41,26 +42,39 @@ class SOM:
             if self.learningRate < 0.001:
                 break
 
-            # if i%500 == 0:
-            #     plt.plot(self.NN[:,0], self.NN[:,1])
-            #     plt.scatter(self.POINTS[:,0], self.POINTS[:,1], edgecolors='r', marker='o',s=3)
-            #     plt.show(block=True)
+            if report:
+                if i%1000 == 0:
+                    name = f"{reportPath}/epoch {i}.png"
+                    self._plotResult(name)
 
+        if report:
 
-        # plt.plot(self.NN[:,0], self.NN[:,1])
-        # plt.scatter(self.POINTS[:,0], self.POINTS[:,1], edgecolors='r', marker='o',s=3)
-        # plt.show(block=True)
+            # self._makeGif(frameFolder="report/*.png", saveName=f"{reportPath}/solution.gif")
+            self._makeGif(saveName=f"{reportPath}/solution.gif")
+            self._printReport()
+
         return self.NN
+    
 
-    def randomSample(self) -> np.array:
+    def _normalize(self, array: np.ndarray) -> np.ndarray:
+        self.norms = np.linalg.norm(array, axis=0)
+        return array / self.norms
+    
+
+    def _initNetwork(self) -> np.ndarray:
+        self.NN = np.random.rand(self.nnSize, 2)
+        return self.NN
+    
+
+    def _randomSample(self) -> np.array:
         return self.POINTS[np.random.choice(len(self.POINTS), size=1, replace=False)]
     
 
-    def selectWinner(self, point: np.array) -> int:
+    def _selectWinner(self, point: np.array) -> int:
         return np.linalg.norm(self.NN - point, axis=1).argmin()
     
 
-    def getNeighborhood(self, index: int, radix: int):
+    def _getNeighborhood(self, index: int, radix: int):
         # Impose an upper bound on the radix to prevent NaN and blocks
         if radix < 1:
             radix = 1
@@ -70,15 +84,33 @@ class SOM:
         distances = np.minimum(deltas, self.nnSize - deltas)
 
         return np.exp((-1*distances**2) / (2*(radix**2)))
-
-
-    def getCitiesNormalize(self) -> np.ndarray: # FIXME:  
-        return self.POINTS
     
-    def getNetwork(self) -> np.ndarray:
-        return self.NN
+
+    def _plotPoints(self, saveName: str) -> None:
+        plt.axis('off')
+        # plt.plot(self.POINTS[:,0], self.POINTS[:,1], color="purple", marker='.', markersize=1.5)
+        plt.scatter(self.POINTS[:,0], self.POINTS[:,1], edgecolors='purple', marker='o',s=2.5)
+        plt.savefig(saveName, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
 
 
+    def _plotResult(self, saveName: str) -> None:
+        plt.axis('off')
+        plt.plot(self.NN[:,0], self.NN[:,1], color="cyan")
+        plt.scatter(self.POINTS[:,0], self.POINTS[:,1], edgecolors='purple', marker='o',s=2.5)
+        plt.savefig(saveName, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
 
-    def getShape(self):
-        return(self.NN.shape[0])
+
+    def _printReport(self) -> None:
+        print()
+        print("Done!!!")
+    
+
+    def _makeGif(self, frameFolder: str="report/*.png", saveName: str="solution.gif") -> None:
+        fileNames = [name for name in glob.glob(frameFolder)]
+        fileNames = sorted(fileNames, key=lambda ts : int(re.findall('\d+', ts)[0]))
+        frames = [Image.open(image) for image in fileNames]
+        frame_one = frames[0]
+        frame_one.save(saveName, format="GIF", append_images=frames,
+                       save_all=True, duration=200, loop=1)
--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import math
 
 from SOM import SOM
 
-# Wczytanie pliku CSV
-data = np.loadtxt('uruguay.csv', delimiter=',')
+DATA_PATH = "data/Poland.csv" 
 
-# Wyświetlenie zawartości tablicy NumPy
-# print(data)
+def loadData(path: str) ->np.ndarray:
+    # Read data from CSV
+    data = np.loadtxt(path, delimiter=',')
+    index = data[:,0]
+    x_coordinates = data[:,1]
+    y_coordinates = data[:,2]
 
-index = data[:,0]
-x_coordinates = data[:,1]
-y_coordinates = data[:,2]
+    return np.transpose(np.array([x_coordinates, y_coordinates]))
 
 
-cities = np.transpose(np.array([x_coordinates, y_coordinates]))
+def main():
+    cities = loadData(DATA_PATH)
+    network = SOM(cities, len(cities) * 8, 0.9997,10000)
+    network.train(report=True)
 
-# print(len(cities))
 
-plt.scatter(cities[:,0], cities[:,1], edgecolors='r', marker='o',s=3)
-plt.show(block=True)
-
-network = SOM(cities, len(cities) * 8, 0.9997,30000)
-solution = network.train()
-normCities = network.getCitiesNormalize()
-
-solution = network.denormalize(solution)
-
-plt.plot(solution[:,0], solution[:,1])
-plt.scatter(cities[:,0], cities[:,1], edgecolors='r', marker='o',s=3)
-plt.show(block=True)
+if __name__ == "__main__":
+    main()
